cfd884d3fe799a3f5c204694656bebf944aec531
gaoxs <gaoxs@qianbeiduo.cn>
2016-11-01 00:00:14 +0800

    新增vue脚手架

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App'
+import hello from './components/Hello.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', component: App},
+  {path: '/a', component: hello}
+]
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+})
+
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+
+var app = new Vue({
+  router
+}).$mount('#app')
+
+export default app
