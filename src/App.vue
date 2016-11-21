a3f8f8e863e42c87a9ae3730a87df0b325d7d9fa
gaoxs <704041637@qq.com>
2016-11-21 11:27:37 +0800

    完成计算器公式

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <template>
   <div class="calculator-box">
-    <div class="calculator-text">{{calculatorDefault}}</div>
+    <div class="calculator-text" v-bind:style="wrapFont">{{calculatorDefault}}</div>
     <div class="calculator-key-warp">
       <div class="calculator-key-num">
         <div class="calculator-key-box">
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">AC</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">+/-</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">%</div>
+          <div class="calculator-key calculator-key-default" @click="remove($event)">AC</div>
+          <div class="calculator-key calculator-key-default" @click="relative($event)">+/-</div>
+          <div class="calculator-key calculator-key-default" @click="percentage($event)">%</div>
         </div>
         <div class="calculator-key-box">
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">7</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">8</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">9</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">7</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">8</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">9</div>
         </div>
         <div class="calculator-key-box">
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">4</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">5</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">6</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">4</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">5</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">6</div>
         </div>
         <div class="calculator-key-box">
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">1</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">2</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">3</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">1</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">2</div>
+          <div class="calculator-key calculator-key-default" @click="key($event)">3</div>
         </div>
         <div class="calculator-key-box">
-          <div class="calculator-key calculator-key-xs calculator-key-default" data-value="AC" @click="key($event)">0</div>
-          <div class="calculator-key calculator-key-default" data-value="AC" @click="key($event)">.</div>
+          <div class="calculator-key calculator-key-xs calculator-key-default" @click="key($event)">0</div>
+          <div class="calculator-key calculator-key-default" @click="decimal($event)">.</div>
         </div>
       </div>
       <div class="calculator-key-algorithm">
-        <div class="calculator-key calculator-key-primary" data-value="AC" @click="key($event)">/</div>
-        <div class="calculator-key calculator-key-primary" data-value="AC" @click="key($event)">*</div>
-        <div class="calculator-key calculator-key-primary" data-value="AC" @click="key($event)">-</div>
-        <div class="calculator-key calculator-key-primary" data-value="AC" @click="key($event)">+</div>
-        <div class="calculator-key calculator-key-primary" data-value="AC" @click="key($event)">=</div>
+        <div class="calculator-key calculator-key-primary" @click="division($event)">/</div>
+        <div class="calculator-key calculator-key-primary" @click="multiplication($event)">*</div>
+        <div class="calculator-key calculator-key-primary" @click="subtraction($event)">-</div>
+        <div class="calculator-key calculator-key-primary" @click="addition($event)">+</div>
+        <div class="calculator-key calculator-key-primary" @click="equal($event)">=</div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import FileFooter from './components/template/Footer'
 
   export default {
@@ -47,7 +47,10 @@
     data () {
       return {
         calculatorDefault: '0',
-        calculatorInit: ''
+        calculatorPro: '',
+        calculatorInit: false,
+        calculatorState: 0,
+        wrapFont: null
       }
     },
     components: {
@@ -58,38 +61,103 @@
     },
     methods: {
       key (event) {
-        var _this = this
         if (this.calculatorDefault === '0') {
           this.calculatorDefault = ''
         }
-        if (event.toElement.textContent === 'AC') {
-          this.calculatorDefault = 0
-        } else if (event.toElement.textContent === '+/-') {
-          this.calculatorDefault = this.calculatorDefault * -1
-        } else if (event.toElement.textContent === '%') {
-          this.calculatorDefault = this.calculatorDefault / 100
-        } else if (event.toElement.textContent === '+') {
-          calculate(1)
-        } else if (event.toElement.textContent === '-') {
-          calculate(2)
-        } else if (event.toElement.textContent === '*') {
-          calculate(3)
-        } else if (event.toElement.textContent === '/') {
-          calculate(4)
+        if (this.calculatorDefault.length >= 8) {
+          this.wrapFont = 'font-size:40px;'
+        } else {
+          this.wrapFont = ''
+        }
+        if (this.calculatorDefault.length >= 13) {
+          return
+        }
+        if (this.calculatorState === -1) {
+          this.calculatorDefault = ''
+          this.calculatorState = 0
+        }
+        if (this.calculatorInit === true) {
+          if (Number(this.calculatorPro) === Number(this.calculatorDefault)) {
+            this.calculatorDefault = ''
+          }
+          this.calculatorDefault = this.calculatorDefault + event.toElement.textContent
+          if (this.calculatorState === 1) {
+            this.calculatorPro = Number(this.calculatorPro) + Number(this.calculatorDefault)
+          } else if (this.calculatorState === 2) {
+            this.calculatorPro = Number(this.calculatorPro) - Number(this.calculatorDefault)
+          } else if (this.calculatorState === 3) {
+            this.calculatorPro = Number(this.calculatorPro) * Number(this.calculatorDefault)
+          } else if (this.calculatorState === 4) {
+            this.calculatorPro = Number(this.calculatorPro) / Number(this.calculatorDefault)
+          }
         } else {
           this.calculatorDefault = this.calculatorDefault + event.toElement.textContent
+          this.calculatorPro = this.calculatorDefault
         }
-        function calculate (num) {
-          if (num === 1) {
-            _this.calculatorDefault = Number(_this.calculatorDefault) + Number(event.toElement.textContent)
-          } else if (num === 2) {
-            _this.calculatorDefault = Number(_this.calculatorDefault) - Number(event.toElement.textContent)
-          } else if (num === 3) {
-            _this.calculatorDefault = Number(_this.calculatorDefault) * Number(event.toElement.textContent)
-          } else if (num === 4) {
-            _this.calculatorDefault = Number(_this.calculatorDefault) / Number(event.toElement.textContent)
-          }
+      },
+      addition (event) {
+        // 加
+        this.calculatorState = 1
+        this.isCalculatorInit()
+      },
+      subtraction (event) {
+        // 减
+        this.calculatorState = 2
+        this.isCalculatorInit()
+      },
+      multiplication (event) {
+        // 乘
+        this.calculatorState = 3
+        this.isCalculatorInit()
+      },
+      division (event) {
+        // 除
+        this.calculatorState = 4
+        this.isCalculatorInit()
+      },
+      equal (event) {
+        // 等于
+        this.calculatorDefault = this.calculatorPro
+        if (this.calculatorDefault.toString().length >= 8) {
+          this.wrapFont = 'font-size:40px;'
         }
+        if (this.calculatorDefault.length >= 13) {
+          this.calculatorDefault = this.calculatorDefault.substring(0, 12)
+        }
+        this.isCalculator()
+        this.calculatorState = -1
+      },
+      remove (event) {
+        // 清0
+        this.calculatorDefault = '0'
+        this.wrapFont = ''
+      },
+      relative (event) {
+        // 正负
+        this.calculatorState = 5
+        this.calculatorDefault = Number(this.calculatorDefault) * -1
+      },
+      percentage (event) {
+        // 百分比
+        this.calculatorState = 6
+        if (this.calculatorDefault === '0') {
+          return
+        }
+        this.calculatorDefault = Number(this.calculatorDefault) / 100
+        this.calculatorState = 0
+      },
+      decimal () {
+        // 小数点
+        if (this.calculatorDefault.indexOf('.') > 0) {
+          return
+        }
+        this.calculatorDefault = this.calculatorDefault + '.'
+      },
+      isCalculatorInit () {
+        this.calculatorInit = true
+      },
+      isCalculator () {
+        this.calculatorInit = false
       }
     }
   }
